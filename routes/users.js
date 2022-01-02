8f8a12333d12ebf8458138205fa86ba3c3f47637
Mark <86029787+mj-wd@users.noreply.github.com>
2022-01-02 08:10:13 -0800

    Week 4: HTTPS

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,14 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+    if (req.user.admin === 'true') {
+        res.send('respond with a resource');
+    } else {
+        err = new Error("You are not authorized to perform this operation!");
+        err.status = 403;
+        return next(err);
+    }
 });
 
 router.post('/signup', (req, res) => {
